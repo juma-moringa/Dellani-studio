f16592631509ce8a06d3f3a8d51fa4bc71235cee
juma-moringa <allan.juma@student.moringaschool.com>
2021-04-17 22:33:02 +0300

    modified styles,html and scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+    $("#toggler").click(function(){
+      $("#design-image").toggle();
+      $("#design").toggle();
+    });
+  });
+  $(document).ready(function(){
+    $("#toggler2").click(function(){
+      $("#development-image").toggle();
+      $("#development").toggle();
+    });
+  });
+  $(document).ready(function(){
+    $("#toggler3").click(function(){
+      $("#product-image").toggle();
+      $("#product").toggle();
+    });
+  });
+  
